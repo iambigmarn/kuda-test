84d2702c89cb2bd9230e1758f3e4d471dd4c4e40
Iambigmarn <marnmorgan@gmail.com>
2023-02-04 22:28:49 +0100

    33

--- src/index.js
+++ src/index.js
@@ -7,14 +7,11 @@ import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>
 );
 
