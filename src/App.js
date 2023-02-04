44b715a6f3a27bfcdde6d2a57af8cc40fbc20d40
Iambigmarn <marnmorgan@gmail.com>
2023-02-04 20:22:19 +0100

    12th

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/homePage';
 import "./assets/main.css";
 import { JoinKuda } from './pages/joinKuda';
@@ -6,10 +6,9 @@ import { SignIn } from './pages/signIn';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/kuda-test">
       <Routes>
         <Route path="/" element={<HomePage/>}/>
-        <Route path="/kuda-test" element={<HomePage />} />
         <Route path="/joinKuda" element={<JoinKuda/>}/>
         <Route path="/signIn" element={<SignIn/>}/>
       </Routes>
