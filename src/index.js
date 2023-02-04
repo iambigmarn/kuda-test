b0cfb8cad2307afb9160dd901032531cb686d980
Iambigmarn <marnmorgan@gmail.com>
2023-02-04 21:25:12 +0100

    25

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
